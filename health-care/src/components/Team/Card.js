7868dea9b66a010d7b7fbdde83cdd12dc455c5c8
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-07-12 00:30:30 +0300

    Making component functional with style fixes and some handlers.

--- health-care/src/components/Team/Card.js
+++ health-care/src/components/Team/Card.js
@@ -1,12 +1,40 @@
-export const Card = () => {
+export const Card = ({
+    name,
+    imageUrl,
+    spec,
+    city,
+    description }) => {
+
+    const onDetailsClick = () => {
+        //TODO open details;
+        console.log(1);
+    }
+    const onSocialClick = (e) => {
+        e.preventDefault();
+        //TODO open some link;
+        console.log(2);
+    }
+
     return (
-        <div className="box">
-            <div className="img-box"><img src="images/team1.jpg" alt="pic" /></div>
-            <div className="detail-box">
-                <h5>Hennry</h5>
-                <h6>MBBS</h6>
-                <div className="social_box"><a href="/"> <i className="fa fa-facebook" aria-hidden="true"></i></a> <a href="/"> <i className="fa fa-twitter" aria-hidden="true"></i></a> <a href="/"> <i className="fa fa-linkedin" aria-hidden="true"></i></a> <a href="/"> <i className="fa fa-instagram" aria-hidden="true"></i></a></div>
-            </div>
-        </div>
+        <div className="owl-item active" style={{ width: "240px", marginRight: "15px" }}>
+            <div className="item" >
+                <div className="owl-item active" style={{ width: "240px", marginRight: "15px" }}>
+                    <div className="item"></div>
+                    <div className="box">
+                        <div className="img-box" onClick={onDetailsClick} style={{ cursor: 'pointer' }}><img src={imageUrl} alt="pic" />
+                            <div className="detail-box">
+                                <h5>{name}</h5>
+                                <h6>{spec}</h6>
+                            </div>
+                        </div>
+                        <div className="social_box">
+                            <a href="/" onClick={onSocialClick}> <i className="fa fa-facebook" aria-hidden="true"></i></a>
+                            <a href="/" onClick={onSocialClick}> <i className="fa fa-twitter" aria-hidden="true"></i></a>
+                            <a href="/" onClick={onSocialClick}> <i className="fa fa-linkedin" aria-hidden="true"></i></a>
+                            <a href="/" onClick={onSocialClick}> <i className="fa fa-instagram" aria-hidden="true"></i></a>
+                        </div>
+                    </div>
+                </div>
+            </div></div >
     );
 }
--- health-care/src/components/Team/Team.js
+++ health-care/src/components/Team/Team.js
@@ -1,6 +1,32 @@
 import { Card } from "./Card";
 
 export const Team = () => {
+    const doctors = [
+        {
+            id: 1,
+            name: 'Henry Jhonson',
+            imageUrl: 'https://familydoctor.org/wp-content/uploads/2018/02/41808433_l.jpg',
+            spec: 'Cardiologist',
+            city: 'Sofia',
+            description: 'Dr. Henry Jhonson is cardiologist with 14 years expirience. In his practice he works with over 30 patients in Healthcare Mico.'
+        },
+        {
+            id: 2,
+            name: 'Susan Miler',
+            imageUrl: 'https://images.theconversation.com/files/304957/original/file-20191203-66986-im7o5.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=1200&h=1200.0&fit=crop',
+            spec: 'Surgeon',
+            city: 'Stara Zagora',
+            description: 'Dr. Susan Miler is surgeon with 12 years expirience. In her practice she had done miniscus treatment, heart surgeries, limb stitches, aneurysm surgery and many more.'
+        },
+        {
+            id: 3,
+            name: 'Gabriel Smith',
+            imageUrl: 'https://bswh-p-001-delivery.sitecorecontenthub.cloud/api/public/content/6ddd84df68cb4fd7bd86a719cdf8c8ab?v=a682b4c0',
+            spec: 'GP',
+            city: 'Lovech',
+            description: 'Dr. Gabriel Smith is cardiologist with 6 years expirience. In her practice she works with over 62 families in Healthcare Mico.'
+        },
+    ]
     return (
         <section className="team_section layout_padding">
             <div className="container">
@@ -10,28 +36,13 @@ export const Team = () => {
                 <div className="carousel-wrap">
                     <div className="owl-carousel team_carousel owl-loaded owl-drag">
 
-                        <div className="owl-stage-outer"><div className="owl-stage" style={{ transform: "translate3d(-1420px, 0px, 0px)", transition: "all 0.25s ease 0s", width: "3195px" }}><div className="owl-item cloned" style={{ width: '340px', marginRght: '15px' }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item cloned" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item cloned" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item active" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item active" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item cloned active" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item cloned" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
-                        </div></div><div className="owl-item cloned" style={{ width: "340px", marginRight: "15px" }}><div className="item">
-                            <Card />
+                        <div className="owl-stage-outer" style={{ display: 'flex', justifyContent: 'center', alignItems: 'center' }}>
+                            {doctors.map(e => <Card key={e.id} {...e} />)}
+                        </div>
 
-                        </div></div></div></div><div className="owl-nav disabled"><button type="button" role="presentation" className="owl-prev"><i className="fa fa-angle-left" aria-hidden="true"></i></button><button type="button" role="presentation" className="owl-next"><i className="fa fa-angle-right" aria-hidden="true"></i></button></div><div className="owl-dots disabled"><button className="owl-dot active"><span></span></button></div></div>
+                    </div>
                 </div>
             </div>
-        </section>
+        </section >
     );
 }

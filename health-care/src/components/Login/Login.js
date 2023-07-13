1c14e5f32a5760d48782d40b7134c13666fd4801
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-07-13 10:10:03 +0300

    Making Login and Register forms controller.

--- health-care/src/components/Login/Login.js
+++ health-care/src/components/Login/Login.js
@@ -1,21 +1,32 @@
+import { useState } from 'react';
+
 export const Login = () => {
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    
+    const onChangePass = (e) => {
+        setPass(e.target.value);
+    }
+
     const onSubmitHandler = (e) => {
         e.preventDefault();
         console.log('login');
     }
+
     return (
         <section className="login_section layout_padding container">
             <form onSubmit={onSubmitHandler}>
                 <h4>LOGIN</h4>
                 <div className="form-group col-lg-4">
-                    <label htmlFor="inputPatientName">Email </label>
-                    <input type="text" className="form-control" id="email" name="email" placeholder="John@health.com" />
+                    <label htmlFor="email">Email </label>
+                    <input type="text" className="form-control" id="email" name="email" placeholder="John@health.com" value={email} onChange={onChangeEmail} />
 
-                    <label htmlFor="inputPhone">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" />
-
-                    <label htmlFor="inputSymptoms">Repeat Password</label>
-                    <input type="password" className="form-control" id="repeatPassword" name="repeatPassword" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={pass} onChange={onChangePass} />
 
                     <button className="btn-box btn">Login</button>
                 </div>
--- health-care/src/components/Register/Register.js
+++ health-care/src/components/Register/Register.js
@@ -1,25 +1,41 @@
+import { useState } from 'react';
+
 export const Register = () => {
+
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    const [repeatPass, setRepeatPass] = useState('');
+
+    const onChangeEmail = (e) => {
+        setEmail(e.target.value);
+    }
+    
+    const onChangePass = (e) => {
+        setPass(e.target.value);
+    }
+
+    const onChangeRepeatPass = (e) => {
+        setRepeatPass(e.target.value);
+    }
 
     const onSubmitHandler = (e) => {
         e.preventDefault();
         console.log('Register');
     }
 
-    //TODO: make css;
-
     return (
         <section className="register_section layout_padding container">
             <form onSubmit={onSubmitHandler}>
                 <h4>Register</h4>
                 <div className="form-group col-lg-4">
-                    <label htmlFor="inputPatientName">Email </label>
-                    <input type="text" className="form-control" id="email" name="email" placeholder="John@health.com" />
+                    <label htmlFor="email">Email </label>
+                    <input type="text" className="form-control" id="email" name="email" placeholder="John@health.com" value={email} onChange={onChangeEmail} />
 
-                    <label htmlFor="inputPhone">Password</label>
-                    <input type="password" className="form-control" id="password" name="password" />
+                    <label htmlFor="password">Password</label>
+                    <input type="password" className="form-control" id="password" name="password" value={pass} onChange={onChangePass} />
 
-                    <label htmlFor="inputSymptoms">Repeat Password</label>
-                    <input type="password" className="form-control" id="repeatPassword" name="repeatPassword" />
+                    <label htmlFor="repeatPassword">Repeat Password</label>
+                    <input type="password" className="form-control" id="repeatPassword" name="repeatPassword" value={repeatPass} onChange={onChangeRepeatPass} />
 
                     <button className="btn-box btn">Register</button>
                 </div>

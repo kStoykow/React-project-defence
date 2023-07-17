7f1613c29808720eea2f183f2d6963477c293f76
Kristiqn Stoykow <kristiqn.stoikow11@gmail.com>
2023-07-17 13:50:16 +0300

    Fixing Date picker form.
    Fixing some of the css issues.

--- health-care/src/components/BookAppointment/DatePicker.js
+++ health-care/src/components/BookAppointment/DatePicker.js
@@ -1,28 +1,50 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, forwardRef } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
+import './DatePicker.Modules.css';
 
 const DatePickerComponent = () => {
     const datepickerRef = useRef(null);
 
     useEffect(() => {
         // Set the initial date when the component mounts
-        if (datepickerRef.current) {
-            datepickerRef.current.setOpen(true); // Open the datepicker by default
-            datepickerRef.current.setCaretPosition(0); // Move the cursor to the beginning of the input
+        if (datepickerRef.current && datepickerRef.current.input) {
+            datepickerRef.current.setOpen(false);
+
+            // Move the cursor to the beginning of the input
+            const inputElement = datepickerRef.current.input;
+            inputElement.focus();
+            inputElement.setSelectionRange(0, 0);
         }
     }, []);
 
+    const CustomInput = forwardRef(({ value, onClick }, ref) => (
+        <div className="input-group date" id="inputDate">
+            <input
+                id="inputDate"
+                className="form-control formm"
+                ref={ref}
+                value={value}
+                onClick={onClick}
+                readOnly
+            />
+            <span className="input-group-addon date_icon">
+                <i className="fa fa-calendar" aria-hidden="true"></i>
+            </span>
+        </div>
+    ));
+
     return (
         <div>
-            <DatePickerComponent
+            <DatePicker
                 ref={datepickerRef}
                 selected={new Date()}
-                dateFormat="yyyy-MM-dd"
+                dateFormat="dd-MM-yyyy"
                 autoComplete="off"
-                closeOnScroll={true}
+                closeOnScroll={false}
                 todayButton="Today"
                 onChange={(date) => console.log(date)} // Handle the selected date
+                customInput={<CustomInput />}
             />
         </div>
     );
